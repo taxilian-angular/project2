fab4fed9957e1b223bab98ef01ddc4557827dc1a
Richard Bateman <taxilian@gmail.com>
2014-05-01 14:09:22 -0600

    Add answer key entry for view 2 to illustrate reusing the directive

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,6 +9,25 @@ angular.module('myApp.controllers', [])
         value: "1"
     };
   }])
-  .controller('MyCtrl2', [function() {
 
+
+
+
+  .controller('MyCtrl2', ['$scope', function($scope) {
+    $scope.data = {
+        choices: "ABCDE",
+        value: "1",
+        questions: []
+    };
+    var questions = $scope.data.questions;
+    for (var i = 0; i < 35; ++i) {
+        questions.push({value: ""});
+    }
+
+    $scope.randomize = function($event) {
+        for (var i = 0; i < 35; ++i) {
+            questions[i].value = ""+Math.floor((Math.random() * 5));
+        }
+        $event.preventDefault();
+    };
   }]);
